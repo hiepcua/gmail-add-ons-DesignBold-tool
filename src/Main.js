5fcdbfdf7f49a35066cc725567893f3697a38f98
HiepTV <35910113+ffse1701003@users.noreply.github.com>
2019-03-05 10:13:53 +0700

    Update version 1.1

--- src/Main.js
+++ src/Main.js
@@ -1,142 +1,3 @@
-var DEBUG = true;
-
-function getInsertImageComposeUI(e) {
-    return [buildImageComposeCard()];
-}
-
-function getImageUrls(){
-    return [
-    "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
-    "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
-    "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
-    ];
-}
-
-function getContextualAddOn(event) {
-    event.parameters = { action: "showAddOn" };
-    return dispatchActionInternal_(event, addOnErrorHandler);
-}
-
-function buildImageComposeCard(accessToken) {
-    return dispatchActionInternal_({
-        parameters: {
-            action: "showLogin",
-            pars: accessToken
-        }
-    });
-}
-
-// Function open new window login DesignBold
-function openLoginWindow() {
-    CardService.newAuthorizationAction()
-    .setAuthorizationUrl("https://designbold.com/");
-
-    // CardService.newAuthorizationException()
-    //     .setAuthorizationUrl("https://designbold.com/")
-    //     .setResourceDisplayName("Example Resource")
-    //     .setCustomUiCallback('test')
-    //     .throwException();
-
-    var actionResponse = CardService.newActionResponseBuilder()
-    .setOpenLink(CardService.newOpenLink()
-        .setUrl("https://designbold.com/")
-        .setOpenAs(CardService.OpenAs.OVERLAY)
-        .setOnClose(CardService.OnClose.RELOAD_ADD_ON));
-    return actionResponse.build();
-}
-
-/* Function check login. Access token not expires */
-function isLogin(){
-    var flag = false;
-    var acToken = getAccessTokenUserProperties();
-    /* Kiểm tra xem access token đã hết hạn/ hợp lệ chưa. */
-    if( acToken ){
-
-    }
-
-    return flag;
-}
-
-function applyInsertImageAction(e) {
-    var actionResponse = CardService.newActionResponseBuilder()
-    .setOpenLink(CardService.newOpenLink()
-        .setUrl("https://designbold.com/")
-        .setOpenAs(CardService.OpenAs.FULL_SIZE));
-    return actionResponse.build();
-}
-
-/**
-* Validates and dispatches an action.
-*
-* @param {Event} event - user event to process
-* @param {ErrorHandler} errorHandler - Handles errors, optionally 
-*        returning a card or action response.
-* @return {ActionResponse|UniversalActionResponse|Card} Card or form action
-*/
-function dispatchActionInternal_(event, errorHandler) {
-    if (DEBUG) {
-        // console.time("dispatchActionInternal");
-        // console.log(event);
-    }
-
-    try {
-        var actionName = event.parameters.action;
-        if (!actionName) {
-            throw new Error("Missing action name.");
-        }
-
-        var actionFn = ActionHandlers[actionName];
-        if (!actionFn) {
-            throw new Error("Action not found: " + actionName);
-        }
-
-        return actionFn(event);
-    } catch (err) {
-        // console.error(err);
-        if (errorHandler) {
-            return errorHandler(err);
-        } else {
-            throw err;
-        }
-    } finally {
-        if (DEBUG) {
-            // console.timeEnd("dispatchActionInternal");
-        }
-    }
-}
-
-/**
-* Handle unexpected errors for the main universal action entry points.
-*
-* @param {Error} exception - Exception to handle
-* @return {Card|ActionResponse|UnivseralActionResponse} optional card or action response to render
-*/
-function addOnErrorHandler(err) {
-    var card = addOnErrorHandler(err);
-    return CardService.newUniversalActionResponseBuilder()
-    .displayAddOnCards([card])
-    .build();
-    // if (err instanceof AuthorizationRequiredException) {
-    //     CardService.newAuthorizationException()
-    //     .setAuthorizationUrl(githubClient().authorizationUrl())
-    //     .setResourceDisplayName("GitHub")
-    //     .setCustomUiCallback("handleAuthorizationRequired")
-    //     .throwException();
-    // } else {
-    //     return buildErrorCard({
-    //         exception: err,
-    //         showStackTrace: DEBUG
-    //     });
-    // }
-}
-
-function loginErrorHandler(err) {
-    var card = addOnErrorHandler(err);
-    return CardService.newUniversalActionResponseBuilder()
-    .displayAddOnCards([card])
-    .build();
-}
-
 /*-----------------------------------------------------------------------------------------------*/
 
 /* Get access token in user properties store */
@@ -159,11 +20,22 @@ function setAccessTokenUserProperties(accessToken){
 
 /* Check access token in user properties store, expires or not */
 function checkAccessTokenExpires(accessToken){
-
 }
 
 /* Get refresh token in user properties store */
 function getRefreshTokenUserProperties(){
+    var userProperties = PropertiesService.getUserProperties();
+    var refreshToken = userProperties.getProperty('refreshToken');
+
+    if(refreshToken === 'null'){
+        return false;
+    }else{
+        return refreshToken;
+    }
+}
+
+/* Get refresh token from server by access token */
+function getRefreshTokenByAccessToken(){
     var userProperties = PropertiesService.getUserProperties();
     var refreshToken = userProperties.getProperty('refreshToken');
 
@@ -186,14 +58,170 @@ function checkRefreshToken(){}
 /* Refresh access token */
 function refreshToken5Time(){}
 
+/*-----------------------------------------------------------------------------------------------*/
+function getInsertImageComposeUI(e) {
+    return [buildImageComposeCard()];
+}
+
+function buildImageComposeCard(accessToken) {
+    // console.log(buildImageComposeCard);
+    var service = getOAuthService();
+    if(service.hasAccess()){
+        var card = CardService.newCardBuilder();
+        var action = CardService.newAction().setFunctionName('openLoginWindow');
+        var cardSection = CardService.newCardSection();
+        cardSection.addWidget(CardService.newImage()
+            .setImageUrl("https://ci5.googleusercontent.com/proxy/0bUHItshL0ecZvuWGz1KRhaHPiZtelj-j4pn3QJuRUqTz1gkmSLFIv4kA4wNv5MrgBUqXaoNO2GBjfKiTAvkjRSKPEw3UMOOEiiqSCs18sKH0eUP7dav5yyZjKVPzQhW4wxwnB0FlkRC")
+            .setAltText("DesignBold")
+            );
+    }else{
+        var card = CardService.newCardBuilder();
+        var action = CardService.newAction().setFunctionName('openLoginWindow');
+        var cardSection = CardService.newCardSection();
+        cardSection.addWidget(CardService.newImage()
+            .setImageUrl("https://ci5.googleusercontent.com/proxy/0bUHItshL0ecZvuWGz1KRhaHPiZtelj-j4pn3QJuRUqTz1gkmSLFIv4kA4wNv5MrgBUqXaoNO2GBjfKiTAvkjRSKPEw3UMOOEiiqSCs18sKH0eUP7dav5yyZjKVPzQhW4wxwnB0FlkRC")
+            .setAltText("DesignBold")
+            );
+
+        cardSection.addWidget(CardService.newImage()
+            .setImageUrl("https://ci6.googleusercontent.com/proxy/sZVfTGhXetRx5FCy27CJw0jLx3qgH9lzO7EuLbFBpRYx3SyFJNjy-jxkaUSkEbKe9EDWviHBHB0uCGGe_TD1AxLB_NPIeEsIfmWK6A6ia34b79cdniY_l0QBP8KRnc5jGF9UNt5UZQ")
+            .setAltText("DesignBold")
+            .setOnClickAction(action)
+            );
+    }
+    return card.addSection(cardSection).build();
+}
+
+function getContextualAddOn(e) {
+    // console.log('getContextualAddOn');
+    var accessToken = e.messageMetadata.accessToken;
+    GmailApp.setCurrentMessageAccessToken(accessToken);
+
+    // console.log(accessToken);
+
+    var service = getOAuthService();
+    // console.log(service.hasAccess());
+    if(service.hasAccess(e)){
+        return showSidebar(e);
+    }else{
+        return create3PAuthorizationUi();
+    }
+}
+
+function showSidebar(e){
+    // console.log("showSidebar");
+    var accessToken = e.messageMetadata.accessToken;
+    GmailApp.setCurrentMessageAccessToken(accessToken);
+    // console.log('accessToken');
+
+    var card = CardService.newCardBuilder();
+    var cardHeader = CardService.newCardHeader();
+
+    var buttonSet = CardService.newButtonSet()
+    .addButton(CardService.newTextButton().setText('Logout')
+        .setComposeAction(
+            CardService.newAction()
+            .setFunctionName('composeEmailCallback'),
+            // .setParameters({acToken: accessToken, threadId: threadId}), 
+            CardService.ComposedEmailType.REPLY_AS_DRAFT))
+        // .setOnClickAction(
+        //  CardService.newAction()
+        //  .setFunctionName('logout')
+        //  ))
+    .addButton(CardService.newTextButton().setText('Refresh add-on')
+        .setOnClickAction(
+            CardService.newAction()
+            .setFunctionName('showSidebar')));
+
+    var section_intro = CardService.newCardSection()
+    .addWidget(
+        CardService.newTextParagraph()
+        .setText('<b class="db_title">Your design with DesignBold</b>')
+        )
+    .addWidget(buttonSet);
+
+    return card.setHeader(cardHeader)
+    .addSection(section_intro)
+    .addSection(getListImage())
+    .build();
+}
+
+function getData(){
+    var accessToken = getOAuthService().getAccessToken();
+    var headers_opt = {
+        "Authorization": "Bearer " + accessToken
+    }
+    return accessProtectedResource("https://api.designbold.com/v3/document?owner=me&sort=modified&start=0&limit=15&target=my-design&loc=wp&folder_id=", "GET", headers_opt);
+}
+
+function getListImage(){
+    var jsonData = JSON.parse(getData());
+    var list = jsonData.response;
+    var cardSection = CardService.newCardSection();
+    var i=0;
+    for (var item in list) {
+        var imageUrl = list[item].thumb;
+        var edit_link = list[item].edit_link;
+        if(imageUrl === '') imageUrl = 'https://cdn.designbold.com/web/dbcream/main/images/empty_design.jpg';
+        cardSection
+        .addWidget(
+            CardService.newImage()
+            .setImageUrl(imageUrl)
+            .setOnClickAction(
+                CardService.newAction()
+                .setFunctionName("openDesign")
+                .setParameters({design_url: edit_link})
+                )
+            );
+    }
+
+    return cardSection;
+}
+
+function showOptionImage(){}
+
+function composeEmailCallback(e) {
+    // var accessToken = e.messageMetadata.accessToken;
+    // GmailApp.setCurrentMessageAccessToken(accessToken);
+
+    var draft = GmailApp.getDrafts()[0];
+    var draftId = draft.getId();
+    var draftById = GmailApp.getDraft(draftId);
+    Logger.log(draft.getMessage().getSubject() == draftById.getMessage().getSubject());
+
+    // var draft = thread.createDraftReply('This is a reply');
+    // return CardService.newComposeActionResponseBuilder()
+    // .setGmailDraft(draft)
+    // .build();
+}
+
+/* Open link to edit/design image */
+function openDesign(e){
+    var actionResponse = CardService.newActionResponseBuilder()
+    .setOpenLink(CardService.newOpenLink()
+        .setUrl(e.parameters.design_url)
+        .setOpenAs(CardService.OpenAs.FULL_SIZE)
+        .setOnClose(CardService.OnClose.NOTHING));
+    return actionResponse.build();
+}
+
+/* Function check login. Access token not expires */
+function isLogin(){
+    var flag = false;
+    var acToken = getAccessTokenUserProperties();
+    /* Kiểm tra xem access token đã hết hạn/ hợp lệ chưa. */
+    if( acToken ){
+        flag = true;
+    }
+
+    return flag;
+}
+
 function accessProtectedResource(url, method_opt, headers_opt) {
+    // console.log('accessProtectedResource');
     var service = getOAuthService();
     var maybeAuthorized = service.hasAccess();
     if (maybeAuthorized) {
-        // A token is present, but it may be expired or invalid. Make a
-        // request and check the response code to be sure.
-
-        // Make the UrlFetch request and return the result.
         var accessToken = service.getAccessToken();
         var method = method_opt || 'get';
         var headers = headers_opt || {};
@@ -202,7 +230,7 @@ function accessProtectedResource(url, method_opt, headers_opt) {
         var resp = UrlFetchApp.fetch(url, {
             'headers': headers,
             'method' : method,
-            'muteHttpExceptions': true, // Prevents thrown HTTP exceptions.
+            'muteHttpExceptions': true, 
         });
 
         var code = resp.getResponseCode();
@@ -212,8 +240,6 @@ function accessProtectedResource(url, method_opt, headers_opt) {
             // Not fully authorized for this action.
             maybeAuthorized = false;
         } else {
-            // Handle other response codes by logging them and throwing an
-            // exception.
             console.error("Backend server error (%s): %s", code.toString(),
                 resp.getContentText("utf-8"));
             throw ("Backend server error: " + code);
@@ -221,8 +247,6 @@ function accessProtectedResource(url, method_opt, headers_opt) {
     }
 
     if (!maybeAuthorized) {
-        // Invoke the authorization flow using the default authorization
-        // prompt card.
         CardService.newAuthorizationException()
         .setAuthorizationUrl(service.getAuthorizationUrl())
         .setResourceDisplayName("Display name to show to the user")
@@ -254,11 +278,15 @@ function authCallback(callbackRequest) {
     }
 }
 
-function resetOAuth() {
-    getOAuthService().reset();
+function logout() {
+    // console.log('logout');
+    var service = getOAuthService()
+    service.reset();
+    getContextualAddOn();
 }
 
 function create3PAuthorizationUi() {
+    // console.log("create3PAuthorizationUi");
     var service = getOAuthService();
     var authUrl = service.getAuthorizationUrl();
     var authButton = CardService.newTextButton()
@@ -268,9 +296,7 @@ function create3PAuthorizationUi() {
 
     var promptText =
     'To show you information from your 3P account that is relevant' +
-    ' to the recipients of the email, this add-on needs authorization' +
-    ' to: <ul><li>Read recipients of the email</li>' +
-    '         <li>Read contact information from 3P account</li></ul>.';
+    ' to the recipients of the email, this add-on needs authorization';
 
     var card = CardService.newCardBuilder()
     .setHeader(CardService.newCardHeader()
@@ -282,43 +308,42 @@ function create3PAuthorizationUi() {
         .addWidget(CardService.newButtonSet()
             .addButton(authButton)))
     .build();
-    return [card];
+    return card;
 }
 
 function get3PAuthorizationUrls() {
-    var acToken = getAccessTokenUserProperties();
-    var rfToken = getRefreshTokenUserProperties();
-    // Tạm thời
-    // var dfAccessToken = "b0f99ceb3d596cb8e7152088548c41e981920c0bd92312047fd8e75b9eee440d";
-    // var accessToken = "Z4nYOLdED50Q2WvwG6ly4eJqjb91rkypXB83zagP";
-    // var refreshToken = "2bv0O7ADlj4WkR38mxLB8MdnpP6KozwrVygZNEBq";
-    var headers_opt = {
-        "Authorization": "Bearer Z4nYOLdED50Q2WvwG6ly4eJqjb91rkypXB83zagP"
-    }
-
-    if( acToken !== null && rfToken !== null ){
-        // Đã đăng nhập rồi, cần kiểm tra xem cái token đó đã hết hạn chưa, đã đúng định dạng chưa
-        var status_acToken = checkAccessTokenExpires(acToken);
-        // Nếu hết hạn thực hiện refresh 5 lần.
-        if( status_acToken === 204 ){
-            // refresh token 5 times
-            var status_rfToken = refreshToken5Time(rfToken);
-            if( status_rfToken === 200 ){
-                /* refresh token success. Save new access token.Do something with new access token */
-            }else{ /* 406 || 500 */
-                /* refresh token false. Do something */
-            }
-        }else{
-            // Nếu access token vẫn còn hạn. Do something.
-            // .....
-        }
-    }else{
-        // Trường hợp chưa đăng nhập.
-        // var card = CardService.newCardBuilder();
-        // card.setHeader(CardService.newCardHeader().setTitle('Login'));
-        // card.build();
-        accessProtectedResource("https://api.designbold.com/v3/user/me", "GET", headers_opt);
-    }
+    // var service = getOAuthService();
+    // if(!service.hasAccess()) return create3PAuthorizationUi();
+    //    var acToken = service.getAccessToken();
+    //
+    //    var rfToken = getRefreshTokenUserProperties();
+    //    // Tạm thời
+    //    // var dfAccessToken = "b0f99ceb3d596cb8e7152088548c41e981920c0bd92312047fd8e75b9eee440d";
+    //    // var accessToken = "Z4nYOLdED50Q2WvwG6ly4eJqjb91rkypXB83zagP";
+    //    // var refreshToken = "2bv0O7ADlj4WkR38mxLB8MdnpP6KozwrVygZNEBq";
+    //    var headers_opt = {
+    //        "Authorization": "Bearer Z4nYOLdED50Q2WvwG6ly4eJqjb91rkypXB83zagP"
+    //    }
+    //
+    //    if( acToken !== null ){
+    //        // Đã đăng nhập rồi, cần kiểm tra xem cái token đó đã hết hạn chưa, đã đúng định dạng chưa
+    //        var status_acToken = checkAccessTokenExpires(acToken);
+    //        // Nếu hết hạn thực hiện refresh 5 lần.
+    //        if( status_acToken === 204 ){
+    //            // refresh token 5 times
+    //            var status_rfToken = refreshToken5Time(rfToken);
+    //            if( status_rfToken === 200 ){
+    //                /* refresh token success. Save new access token.Do something with new access token */
+    //            }else{ /* 406 || 500 */
+    //                /* refresh token false. Do something */
+    //            }
+    //        }else{
+    //            // Nếu access token vẫn còn hạn. Do something.
+    //            // .....
+    //        }
+    //    }else{
+    //        return create3PAuthorizationUi();
+    //    }
 
     // accessProtectedResource("https://api.designbold.com/v3/user/me", "GET");
     // accessProtectedResource("https://api.service2.com/probe");
